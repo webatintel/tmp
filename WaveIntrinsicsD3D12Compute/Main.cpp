825aa71733db5ca7163bf52babb1cb65defd83c4
Qin Jiajia <jiajia.qin@intel.com>
2019-08-22 11:38:40 +0800

    Add Wave intrinsces example on d3d12

---
+++ WaveIntrinsicsD3D12Compute/Main.cpp
@@ -0,0 +1,22 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+#include "stdafx.h"
+#include "D3D12SM6WaveIntrinsics.h"
+
+_Use_decl_annotations_
+int main()
+{
+    D3D12SM6WaveIntrinsics sample;
+	sample.Start();
+	return 0;
+  //  return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
